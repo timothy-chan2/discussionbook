c5620e0e97467d5e75dd0768b6ad3c098dfda7f3
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-03-23 17:47:03 -0400

    Add and render sample comments

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Comment } from './Comment';
+
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
+const comments = [
+  {_id: 1, user: '1@email.ca', text: 'First Comment'},
+  {_id: 2, user: '2@email.ca',text: 'Second Comment'},
+  {_id: 3, user: '3@email.ca',text: 'Third Comment'},
+];
+
+const oldComments = comments.map(comment => (
+  <Comment
+    key={comment._id}
+    comment={comment}
+  />
+));
+
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
+    <h1>Discussionbook</h1>
+
+    <article>{oldComments}</article>
+
     <Hello/>
     <Info/>
   </div>
