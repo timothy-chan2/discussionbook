d2843ddcdb4c4c48f010fe8f116a2eb6482ebcbc
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-03-23 17:50:16 -0400

    Remove all references to hello and info component as they are not needed

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Comment } from './Comment';
 
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
-
 const comments = [
   {_id: 1, user: '1@email.ca', text: 'First Comment'},
   {_id: 2, user: '2@email.ca',text: 'Second Comment'},
@@ -22,8 +19,5 @@ export const App = () => (
     <h1>Discussionbook</h1>
 
     <article>{oldComments}</article>
-
-    <Hello/>
-    <Info/>
   </div>
 );
